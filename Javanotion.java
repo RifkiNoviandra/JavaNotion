1347ae13fcfe1436492bced34d529d50fc576b4e
RifkiNoviandra <69407359+RifkiNoviandra@users.noreply.github.com>
2021-07-06 17:04:41 +0700

    Create Javanotion.java

---
+++ Javanotion.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javanotion;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rifki Noviandra
+ */
+public class Javanotion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int limit ;
+        Scanner input = new Scanner (System.in);
+        System.out.print("Input The Limit : ");
+        limit = input.nextInt();
+        if(limit > 0){
+            for (int number = 1 ; number <= limit; number++) 
+            {
+                if((number % 3 == 0) && (number % 5 == 0)){
+                    System.out.print("FizzBuzz" + ",");
+                }
+                if((number%3 == 0) && (number%5 != 0)){
+                    System.out.print("Fizz" + ",");
+                }
+                if((number%5 == 0) && (number%3 != 0)){
+                    System.out.print("Buzz" + ",");
+                }
+                if((number % 3 != 0) && (number % 5 != 0)){
+                    System.out.print(number + ",");
+                }
+            } System.out.println("END");
+        }else{
+            System.out.println("Limit Can't Be Less Than 0");
+        }
+    }
+    
+}
